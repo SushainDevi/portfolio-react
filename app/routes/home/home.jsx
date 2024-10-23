0b4376e25a08f0187665311345d85a58c91504e6
SushainDevi <115884300+SushainDevi@users.noreply.github.com>
2024-10-23 14:31:55 +0530

    first commit

---
+++ app/routes/home/home.jsx
@@ -0,0 +1,175 @@
+import gamestackTexture2Large from '~/assets/gamestack-list-large.jpg';
+import gamestackTexture2Placeholder from '~/assets/gamestack-list-placeholder.jpg';
+import gamestackTexture2 from '~/assets/gamestack-list.jpg';
+import gamestackTextureLarge from '~/assets/gamestack-login-large.jpg';
+import gamestackTexturePlaceholder from '~/assets/gamestack-login-placeholder.jpg';
+import gamestackTexture from '~/assets/gamestack-login.jpg';
+import sliceTextureLarge from '~/assets/slice-app-large.jpg';
+import sliceTexturePlaceholder from '~/assets/slice-app-placeholder.jpg';
+import sliceTexture from '~/assets/slice-app.jpg';
+import sprTextureLarge from '~/assets/spr-lesson-builder-dark-large.png';
+import sprTexturePlaceholder from '~/assets/spr-lesson-builder-dark-placeholder.jpg';
+import sprTexture from '~/assets/spr-lesson-builder-dark.png';
+import { Footer } from '~/components/footer';
+import { baseMeta } from '~/utils/meta';
+import { Intro } from './intro';
+import { Profile } from './profile';
+import { ProjectSummary } from './project-summary';
+import { useEffect, useRef, useState } from 'react';
+import config from '~/config.json';
+import styles from './home.module.css';
+
+// Prefetch draco decoader wasm
+export const links = () => {
+  return [
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_wasm_wrapper.js',
+      as: 'script',
+      type: 'text/javascript',
+      importance: 'low',
+    },
+    {
+      rel: 'prefetch',
+      href: '/draco/draco_decoder.wasm',
+      as: 'fetch',
+      type: 'application/wasm',
+      importance: 'low',
+    },
+  ];
+};
+
+export const meta = () => {
+  return baseMeta({
+    title: 'AI Developer + Engineer',
+    description: `Portfolio of ${config.name} — an aspiring AI developer focused on AI-driven solutions, machine learning, natural language processing, and creating seamless user experiences through innovative technology.`,
+  });
+};
+
+
+export const Home = () => {
+  const [visibleSections, setVisibleSections] = useState([]);
+  const [scrollIndicatorHidden, setScrollIndicatorHidden] = useState(false);
+  const intro = useRef();
+  const projectOne = useRef();
+  const projectTwo = useRef();
+  const projectThree = useRef();
+  const details = useRef();
+
+  useEffect(() => {
+    const sections = [intro, projectOne, projectTwo, projectThree, details];
+
+    const sectionObserver = new IntersectionObserver(
+      (entries, observer) => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const section = entry.target;
+            observer.unobserve(section);
+            if (visibleSections.includes(section)) return;
+            setVisibleSections(prevSections => [...prevSections, section]);
+          }
+        });
+      },
+      { rootMargin: '0px 0px -10% 0px', threshold: 0.1 }
+    );
+
+    const indicatorObserver = new IntersectionObserver(
+      ([entry]) => {
+        setScrollIndicatorHidden(!entry.isIntersecting);
+      },
+      { rootMargin: '-100% 0px 0px 0px' }
+    );
+
+    sections.forEach(section => {
+      sectionObserver.observe(section.current);
+    });
+
+    indicatorObserver.observe(intro.current);
+
+    return () => {
+      sectionObserver.disconnect();
+      indicatorObserver.disconnect();
+    };
+  }, [visibleSections]);
+
+  return (
+    <div className={styles.home}>
+      <Intro
+        id="intro"
+        sectionRef={intro}
+        scrollIndicatorHidden={scrollIndicatorHidden}
+      />
+      <ProjectSummary
+        id="project-1"
+        sectionRef={projectOne}
+        visible={visibleSections.includes(projectOne.current)}
+        index={1}
+        title="DocChat: AI-Powered Document Summarization"
+        description="Developed an AI-driven system to automate document text extraction, cleaning, and summarization, integrated with a chatbot interface for seamless interaction with uploaded documents."
+        buttonText="View project"
+        buttonLink="/projects/smart-sparrow"
+        model={{
+          type: 'laptop',
+          alt: 'DocChat Interface',
+          textures: [
+            {
+              srcSet: `${sprTexture} 1280w, ${sprTextureLarge} 2560w`,
+              placeholder: sprTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-2"
+        alternate
+        sectionRef={projectTwo}
+        visible={visibleSections.includes(projectTwo.current)}
+        index={2}
+        title="Personalized Music Recommendation System"
+        description="Built a hybrid recommendation system leveraging the Spotify API, combining content-based filtering with popularity-based scoring to provide personalized music recommendations."
+        buttonText="View project"
+        buttonLink="/projects/volkihar-knight"
+        model={{
+          type: 'phone',
+          alt: 'Music Recommendation System interface',
+          textures: [
+            {
+              srcSet: `${gamestackTexture} 375w, ${gamestackTextureLarge} 750w`,
+              placeholder: gamestackTexturePlaceholder,
+            },
+            {
+              srcSet: `${gamestackTexture2} 375w, ${gamestackTexture2Large} 750w`,
+              placeholder: gamestackTexture2Placeholder,
+            },
+          ],
+        }}
+      />
+      <ProjectSummary
+        id="project-3"
+        sectionRef={projectThree}
+        visible={visibleSections.includes(projectThree.current)}
+        index={3}
+        title="LLaMA-3-Like Transformer Model Implementation"
+        description="Developed a transformer-based large language model, inspired by the LLaMA-3 architecture. Implemented components such as tokenization, rotary positional encoding, and attention mechanisms to enhance text generation performance."
+        buttonText="View project"
+        buttonLink="/projects/slice"
+        model={{
+          type: 'laptop',
+          alt: 'LLaMA-3 Transformer Model Interface',
+          textures: [
+            {
+              srcSet: `${sliceTexture} 800w, ${sliceTextureLarge} 1920w`,
+              placeholder: sliceTexturePlaceholder,
+            },
+          ],
+        }}
+      />
+      <Profile
+        sectionRef={details}
+        visible={visibleSections.includes(details.current)}
+        id="details"
+      />
+      <Footer />
+    </div>
+  );
+};
