4e4c3fc9ca2da23d585894b625cdfaa5c415597a
SushainDevi <115884300+SushainDevi@users.noreply.github.com>
2024-10-24 17:54:11 +0530

    Add build folder

---
+++ build/client/assets/config-jEIqSiHV.js
@@ -0,0 +1,6 @@
+const i="Sushain Devi",n="AI Developer",s=["ML Enthusiast","NLP Specialist","GEN AI Explorer","Prototyper"],o="https://sushain.devi",t="sushaindevi",e="SushainDevi",c="https://github.com/SushainDevi/portfolio",_=` ____
+/ ___|
+\\___ \\
+ ___) |
+|____/
+`,l={name:i,role:n,disciplines:s,url:o,linkedin:t,github:e,portfolio:c,ascii:_};export{l as c};
---
+++ build/client/assets/spr-lesson-builder-dark-rMeTAqVR.js
@@ -0,0 +1,1 @@
+const s="/assets/spr-lesson-builder-dark-large-GERc5Kou.png",r="/assets/spr-lesson-builder-dark-placeholder-BYjrS8rr.jpg",e="/assets/spr-lesson-builder-dark-CTVjjtHW.png";export{s as a,r as b,e as s};
